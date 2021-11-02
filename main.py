1fb0895df81f8a50e5361224ea8aa9e13919c7ff
SRIRAKSHA-SARATHI <st64@njit.edu>
2021-11-02 08:43:06 -0400

    Updated the main.py file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """ content of calculator.py#"""
 
 def inc(x_value):
-    """ Increment Function adds one to the x_value"""
+    """ Increment Function adds one to the value of x"""
     return x_value + 1
 def test_answer():
     """This Tests the function"""
